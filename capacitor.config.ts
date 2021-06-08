72b26a188f7ac2c929902135131ef4fd1c0b09c5
R8-torlo <rubentorli.rt@gmail.com>
2021-06-08 17:43:29 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'com.laserando.orders',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
